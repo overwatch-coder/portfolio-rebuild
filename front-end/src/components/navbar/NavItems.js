80822979b519a99aa52200d04d82f5c7046546f7
brainy <devbrainy98@gmail.com>
2022-08-02 22:07:20 +0100

    modified jumpto links

--- front-end/src/components/navbar/NavItems.js
+++ front-end/src/components/navbar/NavItems.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const NavItems = () => {
   return (
     <>
-    <Link to={"/"} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Home</Link>
+    <AnchorLink href='#home' className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Home</AnchorLink>
     <AnchorLink href='#about' className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">About</AnchorLink>
     <AnchorLink href='#services' className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Services</AnchorLink>
     <AnchorLink href='#portfolio' className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Portfolio</AnchorLink>
