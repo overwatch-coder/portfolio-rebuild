406a3754c8ce1e650c3532b682d6acf339d668d3
brainy <devbrainy98@gmail.com>
2022-08-02 21:42:49 +0100

    modified jumpto links

--- front-end/src/components/navbar/NavItems.js
+++ front-end/src/components/navbar/NavItems.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const NavItems = () => {
-  const jumpToFile = (url) => {
+  const jumpToFile = (event, url) => {
+    event.preventDefault();
     window.open(url, "_self");
   }
 
 
   return (
     <>
-    <Link to={'#'} onClick={jumpToFile('#home')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Home</Link>
-    <Link to={'#'} onClick={jumpToFile('#about')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">About</Link>
-    <Link to={'#'} onClick={jumpToFile('#services')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Services</Link>
-    <Link to={'#'} onClick={jumpToFile('#portfolio')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Portfolio</Link>
-    <Link to={'#'} onClick={jumpToFile('#contact')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Contact</Link>
+    <Link to={'#'} onClick={(event) => jumpToFile(event, '/#home')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Home</Link>
+    <Link to={'#'} onClick={(event) => jumpToFile(event, '/#about')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">About</Link>
+    <Link to={'#'} onClick={(event) => jumpToFile(event, '/#services')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Services</Link>
+    <Link to={'#'} onClick={(event) => jumpToFile(event, '/#portfolio')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Portfolio</Link>
+    <Link to={'#'} onClick={(event) => jumpToFile(event, '/#contact')} className="uppercase text-sm px-3 rounded transition py-2 text-light/70 hover:text-light hover:bg-cyan-600 w-full md:hover:bg-transparent md:hover:text-cyan-600">Contact</Link>
     </>
   )
 }
